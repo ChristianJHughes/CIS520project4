a2033ba970ce96934dd55af03ce446565b501360
Christian Hughes <chughes.mail@icloud.com>
2017-04-23 18:42:38 -0500

    Commit completed base case

--- solutions/base/base_solution_1.c
+++ solutions/base/base_solution_1.c
@@ -3,10 +3,10 @@
 #include <string.h>
 #include <sys/time.h>
 
-#define WIKI_ARRAY_SIZE 1000000
-#define WIKI_STRING_SIZE 2000
-#define WORDS_ARRAY_SIZE 50000
-#define WORDS_STRING_SIZE 9
+#define WIKI_ARRAY_SIZE 50 /* Should be 1000000 */
+#define WIKI_STRING_SIZE 2003
+#define WORDS_ARRAY_SIZE 75 /* Should be 50000 */
+#define WORDS_STRING_SIZE 10
 
 char wiki_array[WIKI_ARRAY_SIZE][WIKI_STRING_SIZE];
 char words_array[WORDS_ARRAY_SIZE][WORDS_STRING_SIZE];
@@ -33,7 +33,7 @@ void read_to_memory()
         line_num++;
     }
     fclose(file);
-
+    free(line);
     printf("Load Memory: %s, %d\n", fileName, line_num);
 
     /* Read the words list to memory line by line. */
@@ -48,14 +48,13 @@ void read_to_memory()
     line_num = 0;
     line = malloc(WORDS_STRING_SIZE);
     while (fgets(line, WORDS_STRING_SIZE, file) != NULL) {
+        line[strcspn(line, "\n")] = 0;
         strcpy(words_array[line_num], line);
-	printf("%s\n", words_array[line_num]);
+	      // printf("%s\n", words_array[line_num]);
         line_num++;
     }
     fclose(file);
-
-    if(line)
-      free(line);
+    free(line);
 
     printf("Load Memory: %s, %d\n", fileName, line_num);
 }
@@ -63,105 +62,42 @@ void read_to_memory()
 void find_word_in_wiki()
 {
   int i, j;
-  for(i = 0; i < WIKI_ARRAY_SIZE; i++){
-    for(j = 0; j < WORDS_ARRAY_SIZE; j++){
-      if(strstr(wiki_array[i], words_array[j]) != NULL) {
-  	printf("%s, %d\n", words_array[j], i);
+  int found_word = 0; // Assume that the words has not been found (false).
+
+  for(i = 0; i < WORDS_ARRAY_SIZE; i++)
+  {
+    found_word = 0;
+    for(j = 0; j < WIKI_ARRAY_SIZE; j++)
+    {
+      char *p = strstr(wiki_array[j], words_array[i]);
+      if(p)
+      {
+        if (found_word == 0)
+        {
+          found_word = 1; // Set found_word to true.
+          printf("%s: %d", words_array[i], j);
+        }
+        else
+        {
+          printf(", %d", j);
+        }
       }
     }
+    if (found_word) printf("\n");
   }
 }
 
-
-// char getRandomChar()
-// {
-// 	int randNum = 0;
-// 	char randChar = ' ';
-
-// 	randNum = 26 * (rand() / (RAND_MAX + 1.0)); 	// pick number 0 < # < 25
-// 	randNum = randNum + 97;				// scale to 'a'
-// 	randChar = (char) randNum;
-
-// 	// printf("%c", randChar);
-// 	return randChar;
-// }
-
-// void init_arrays()
-// {
-//   int i, j, randNum; 
-//   char randChar;
-
-//   for ( i = 0; i < ARRAY_SIZE; i++) {
-// 	for ( j = 0; j < STRING_SIZE; j++ ) {
-// 		 char_array[i][j] = getRandomChar();
-// 	}
-//   }
-
-//   for ( i = 0; i < 26; i++ ) {
-//   	char_counts[i] = 0;
-//   }
-// }
-
-// void count_array()
-// {
-//   char theChar;
-//   int i, j, charLoc;
-
-//   for ( i = 0; i < ARRAY_SIZE; i++) {
-// 	for ( j = 0; j < STRING_SIZE; j++ ) {
-// 	         theChar = char_array[i][j];
-// 		 charLoc = ((int) theChar) - 97;
-// 		 char_counts[charLoc]++;
-// 	}
-//   }
-// }
-
-// void print_results()
-// {
-//   int i, total = 0;
-
-//   for ( i = 0; i < 26; i++ ) {
-//      total += char_counts[i];
-//      printf(" %c %d\n", (char) (i + 97), char_counts[i]);
-//   }
-//   printf("\nTotal characters:  %d\n", total);
-// }
-
 int main() {
-
 
 	gettimeofday(&t1, NULL);
 
-	
-	printf("DEBUG: starting loop on %s\n", getenv("HOST"));
-
 	//DO PROCESSING HERE
-	//init_arrays();
 	read_to_memory();
 	find_word_in_wiki();
-	//count_array();
-	//print_results();
 
 	gettimeofday(&t2, NULL);
-
 
 	elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0; //sec to ms
 	elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0; // us to ms
 	printf("DATA, %d, %s, %f\n", myVersion, getenv("NSLOTS"),  elapsedTime);
-
-
-
-
-	
 }
-
-
-
-
-
-
-
-
-
-
-
