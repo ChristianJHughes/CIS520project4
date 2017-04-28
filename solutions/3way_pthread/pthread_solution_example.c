554092ac314831fc6972c24163f2f5efe441c04f
Matt Hixon <mhixon333@sbcglobal.net>
2017-04-28 16:00:37 -0500

    pthread example complete

--- solutions/3way_pthread/pthread_solution_example.c
+++ solutions/3way_pthread/pthread_solution_example.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/time.h>
+#include <pthread.h>
 
 #define WIKI_ARRAY_SIZE 50 /* The number of wiki entries. Should be 1000000 for production. */
 #define WIKI_STRING_SIZE 2003 /* The number of characters in each wiki entry. Must account for newline and terminating characters. */
@@ -16,6 +17,8 @@ char words_array[WORDS_ARRAY_SIZE][WORDS_STRING_SIZE];
 
 /* Results of the word search*/
 char results_array[WORDS_ARRAY_SIZE][WIKI_ARRAY_SIZE];
+
+int num_threads = 2;
 
 /* Initialize the results array to all zero */
 void init_array()
@@ -36,7 +39,8 @@ int read_to_memory()
 {
   /* Read the wiki article into memory line by line. */
   // FILE *file = fopen("/scratch/dan/wiki.1Mshort", "r");
-  FILE *file = fopen("/homes/cjhughes255/project4/wiki.50short", "r");
+  // FILE *file = fopen("/homes/cjhughes255/project4/wiki.50short", "r");
+  FILE *file = fopen("../../wiki.50short", "r");
 
   if(file == NULL)
     {
@@ -56,7 +60,8 @@ int read_to_memory()
 
   /* Read the words list to memory line by line. */
   // file = fopen("/scratch/dan/words_4-8chars_50k", "r");
-  file = fopen("/homes/cjhughes255/project4/words_4-8chars75", "r");
+  // file = fopen("/homes/cjhughes255/project4/words_4-8chars75", "r");
+  file = fopen("../../words_4-8chars75", "r");
   
   if(file == NULL)
     {
@@ -78,21 +83,30 @@ int read_to_memory()
 }
 
 /* If a given word is present in 1 or more wiki articles, make note of that in results_array */
-void find_word_in_wiki()
+void *find_word_in_wiki(void *myID)
 {
   int i, j;
 
-  for(j = 0; j < WIKI_ARRAY_SIZE; j++)
+  int startPos = ((int) myID) * (WIKI_ARRAY_SIZE / num_threads);
+  int endPos = startPos + (WIKI_ARRAY_SIZE / num_threads);
+
+  if((int)myID == num_threads-1)
+  {
+    endPos = WIKI_ARRAY_SIZE;
+  }
+
+  for(i = 0; i < WORDS_ARRAY_SIZE; i++)
+  {
+    for(j = startPos; j < endPos; j++)
     {
-      for(i = 0; i < WORDS_ARRAY_SIZE; i++)
-	{
-	  char *p = strstr(wiki_array[j], words_array[i]);
-	  if(p)
-	    {
-	      results_array[i][j] = 't';
-	    }
-	}
+      char *p = strstr(wiki_array[j], words_array[i]);
+      if(p)
+      {
+        results_array[i][j] = 't';
+      }
     }
+  }
+  pthread_exit(NULL);
 }
 
 /* Print out found words and their associated articles */
@@ -129,40 +143,76 @@ void print_results()
 }
 
 int main() {
+
+
+
   /* For measuring performance. */
   struct timeval t1, t2, t3, t4, t5;
   double elapsedTime;
-  int numSlots, myVersion = 1;
+  int numSlots, myVersion = 2; //pthreads = 2
+
+
+  int i, rc;
+  pthread_t threads[num_threads];
+  pthread_attr_t attr;
+  void *status;
+
+  /* Initialize and set thread detached attribute */
+  pthread_attr_init(&attr);
+  pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
 
   gettimeofday(&t1, NULL);
   init_array();
   gettimeofday(&t2, NULL);
+  
+  if(read_to_memory() == 0) {
+    gettimeofday(&t3, NULL);
+
+    for (i = 0; i < num_threads; i++ ) {
+          rc = pthread_create(&threads[i], &attr, find_word_in_wiki, (void *)i);
+          if (rc) {
+            printf("ERROR; return code from pthread_create() is %d\n", rc);
+            exit(-1);
+          }
+    }
+
+    /* Free attribute and wait for the other threads */
+    pthread_attr_destroy(&attr);
+    for(i=0; i<num_threads; i++) {
+         rc = pthread_join(threads[i], &status);
+         if (rc) {
+          printf("ERROR; return code from pthread_join() is %d\n", rc);
+          exit(-1);
+         }
+    }
+    gettimeofday(&t4, NULL);
+
+
+    print_results();
+  }
+  else {
+    return -1;
+  }
+  gettimeofday(&t5, NULL);
+  
+
   elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0; //sec to ms
   elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0; // us to ms
   printf("Time to Init Array: %f\n", elapsedTime);
 
-  // Read file into memory and print out all of the found words.
-  if (read_to_memory() == 0) {
-    gettimeofday(&t3, NULL);
-    elapsedTime = (t3.tv_sec - t2.tv_sec) * 1000.0; //sec to ms
-    elapsedTime += (t3.tv_usec - t2.tv_usec) / 1000.0; // us to ms
-    printf("Time to read: %f\n", elapsedTime);
-    find_word_in_wiki();
+  elapsedTime = (t3.tv_sec - t2.tv_sec) * 1000.0; //sec to ms
+  elapsedTime += (t3.tv_usec - t2.tv_usec) / 1000.0; // us to ms
+  printf("Time to read: %f\n", elapsedTime);
 
-    gettimeofday(&t4, NULL);
-    elapsedTime = (t4.tv_sec - t3.tv_sec) * 1000.0; //sec to ms
-    elapsedTime += (t4.tv_usec - t3.tv_usec) / 1000.0; // us to ms
-    printf("Time to search: %f\n", elapsedTime);
-
-    print_results();
-  }
-  else
-    {
-      return -1;
-    }
-  gettimeofday(&t5, NULL);
+  elapsedTime = (t4.tv_sec - t3.tv_sec) * 1000.0; //sec to ms
+  elapsedTime += (t4.tv_usec - t3.tv_usec) / 1000.0; // us to ms
+  printf("Time to search: %f\n", elapsedTime);
 
   elapsedTime = (t5.tv_sec - t1.tv_sec) * 1000.0; //sec to ms
   elapsedTime += (t5.tv_usec - t1.tv_usec) / 1000.0; // us to ms
-  printf("DATA, %d, %s, %f\n", myVersion, getenv("NSLOTS"),  elapsedTime);
+  printf("DATA, %d, %s, %f, %d\n", myVersion, getenv("NSLOTS"),  elapsedTime, num_threads);
+  
+  printf("Main: program completed. Exiting.\n");
+  pthread_exit(NULL);
 }
